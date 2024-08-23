cfd8008ab28d5db94f25590aac001b2baacef717
adohong4 <adohong4@gmail.com>
2024-08-23 16:06:32 +0700

    handler

--- movieAPI/src/main/java/com/movieflix/movieAPI/dto/MovieDto.java
+++ movieAPI/src/main/java/com/movieflix/movieAPI/dto/MovieDto.java
@@ -1,43 +1,35 @@
-package com.movieflix.movieAPI.entity;
+package com.movieflix.movieAPI.dto;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
-@Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-public class Movie {
+public class MovieDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer movierId;
 
-    @Column(nullable = false, length = 200)
     @NotBlank(message = "Please provide movie's title!")
     private String title;
 
-    @Column(nullable = false)
     @NotBlank(message = "Please provide movie's director!")
     private String director;
 
-    @Column(nullable = false)
     @NotBlank(message = "Please provide movie's studio!")
     private String studio;
 
-    @ElementCollection
-    @CollectionTable(name = "movie_cast")
     private Set<String> movieCast;
 
-    @Column(nullable = false)
     private Integer releaseYear;
 
-    @Column(nullable = false)
     @NotBlank(message = "Please provide movie's poster!")
     private String poster;
+
+    @NotBlank(message = "Please provide poster's url!")
+    private String posterUrl;
 }
